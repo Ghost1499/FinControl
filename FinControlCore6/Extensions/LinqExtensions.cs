94c0f517f811844366589a28ece08eee33a77e84
Ghost1499 <zgstv@yandex.ru>
2022-09-02 23:39:42 +0300

    add linq extension to dinamic ordering

---
+++ FinControlCore6/Extensions/LinqExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FinControlCore6.Models.AuxiliaryModels;
+using System.Linq.Expressions;
+using System;
+using System.Linq;
+
+namespace FinControlCore6.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IQueryable<T> OrderByDynamic<T>(
+            this IQueryable<T> query,
+            string orderByMember,
+            DataTableOrderDir direction)
+        {
+            var param = Expression.Parameter(typeof(T), "c");
+
+            var body = orderByMember.Split('.').Aggregate<string, Expression>(param, Expression.PropertyOrField);
+
+            var queryable = direction == DataTableOrderDir.Asc ?
+                (IOrderedQueryable<T>)Queryable.OrderBy(query.AsQueryable(), (dynamic)Expression.Lambda(body, param)) :
+                (IOrderedQueryable<T>)Queryable.OrderByDescending(query.AsQueryable(), (dynamic)Expression.Lambda(body, param));
+
+            return queryable;
+        }
+
+
+    }
+}
